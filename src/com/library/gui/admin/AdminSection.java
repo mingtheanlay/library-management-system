cd8a262dbf255f7389fdbfcf6944e107b079ffa3
Thean <mingthean.lay@gmail.com>
2020-02-18 23:50:35 +0700

    admin section

---
+++ src/com/library/gui/admin/AdminSection.java
@@ -0,0 +1,118 @@
+package com.library.gui.admin;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AdminSection {
+
+	private JFrame adminFrame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminSection window = new AdminSection();
+					window.adminFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AdminSection() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		adminFrame = new JFrame();
+		adminFrame.setResizable(false);
+		adminFrame.setBackground(Color.WHITE);
+		adminFrame.setBounds(100, 100, 720, 512);
+		adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		adminFrame.getContentPane().setLayout(null);
+		
+		JPanel navPanel = new JPanel();
+		navPanel.setBackground(new Color(106, 90, 205));
+		navPanel.setBounds(0, 0, 226, 490);
+		adminFrame.getContentPane().add(navPanel);
+		navPanel.setLayout(null);
+		
+		JLabel lbAdminSection = new JLabel("ADMIN SECTION");
+		lbAdminSection.setBackground(new Color(255, 255, 255));
+		lbAdminSection.setBounds(19, 66, 184, 33);
+		navPanel.add(lbAdminSection);
+		lbAdminSection.setFont(new Font("Roboto", Font.BOLD, 24));
+		lbAdminSection.setForeground(new Color(255, 255, 255));
+		
+		JButton btnRemoveLibrarian = new JButton("Remove Librarian");
+		btnRemoveLibrarian.setFont(new Font("Roboto Condensed", Font.PLAIN, 20));
+		btnRemoveLibrarian.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRemoveLibrarian.setBounds(0, 267, 226, 45);
+		navPanel.add(btnRemoveLibrarian);
+		btnRemoveLibrarian.setForeground(new Color(255, 255, 255));
+		btnRemoveLibrarian.setBackground(new Color(106, 90, 205));
+		
+			btnRemoveLibrarian.setOpaque(false);
+			btnRemoveLibrarian.setContentAreaFilled(false);
+			btnRemoveLibrarian.setBorderPainted(false);
+		
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		btnLogout.setFont(new Font("Roboto Mono", Font.PLAIN, 16));
+		btnLogout.setBounds(59, 398, 104, 45);
+		navPanel.add(btnLogout);
+		
+		JButton btnViewLibrarian = new JButton("View Librarian");
+		btnViewLibrarian.setOpaque(false);
+		btnViewLibrarian.setForeground(Color.WHITE);
+		btnViewLibrarian.setFont(new Font("Roboto Condensed", Font.PLAIN, 20));
+		btnViewLibrarian.setContentAreaFilled(false);
+		btnViewLibrarian.setBorderPainted(false);
+		btnViewLibrarian.setBackground(new Color(106, 90, 205));
+		btnViewLibrarian.setBounds(0, 209, 226, 45);
+		navPanel.add(btnViewLibrarian);
+		
+		JButton btnAddLibrarian = new JButton("Add Librarian");
+		btnAddLibrarian.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAddLibrarian.setOpaque(false);
+		btnAddLibrarian.setForeground(Color.WHITE);
+		btnAddLibrarian.setFont(new Font("Roboto Condensed", Font.PLAIN, 20));
+		btnAddLibrarian.setContentAreaFilled(false);
+		btnAddLibrarian.setBorderPainted(false);
+		btnAddLibrarian.setBackground(new Color(106, 90, 205));
+		btnAddLibrarian.setBounds(0, 152, 226, 45);
+		navPanel.add(btnAddLibrarian);
+		
+	}
+}
--- src/com/library/gui/login/LoginForm.java
+++ src/com/library/gui/login/LoginForm.java
@@ -1,6 +1,7 @@
-package com.library.gui;
+package com.library.gui.login;
 
 import java.awt.Color;
+
 import java.awt.EventQueue;
 import java.awt.Font;
 
@@ -10,10 +11,14 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class App {
+import com.library.gui.admin.*;
 
-	private JFrame frame;
+public class LoginForm {
+
+	private JFrame loginFrame;
 
 	/**
 	 * Launch the application.
@@ -22,8 +27,8 @@ public class App {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					App window = new App();
-					window.frame.setVisible(true);
+					LoginForm window = new LoginForm();
+					window.loginFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -34,7 +39,7 @@ public class App {
 	/**
 	 * Create the application.
 	 */
-	public App() {
+	public LoginForm() {
 		initialize();
 	}
 
@@ -42,48 +47,58 @@ public class App {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 800, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		loginFrame = new JFrame();
+		loginFrame.setBackground(Color.WHITE);
+		loginFrame.setTitle("Library Management System");
+		loginFrame.setResizable(false);
+		loginFrame.getContentPane().setBackground(Color.WHITE);
+		loginFrame.setBounds(100, 100, 800, 400);
+		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		loginFrame.getContentPane().setLayout(null);
 		
 		JLabel lbTitlte = new JLabel("Library Management System");
 		lbTitlte.setFont(new Font("Roboto Mono", Font.PLAIN, 36));
 		lbTitlte.setHorizontalAlignment(SwingConstants.CENTER);
 		lbTitlte.setBounds(115, 22, 580, 85);
-		frame.getContentPane().add(lbTitlte);
+		loginFrame.getContentPane().add(lbTitlte);
 		
 		JLabel lbAccountName = new JLabel("Account Name");
 		lbAccountName.setFont(new Font("Roboto Condensed", Font.PLAIN, 18));
 		lbAccountName.setBounds(183, 146, 112, 27);
-		frame.getContentPane().add(lbAccountName);
+		loginFrame.getContentPane().add(lbAccountName);
 		
 		JLabel lbPassword = new JLabel("Password");
 		lbPassword.setFont(new Font("Roboto Condensed", Font.PLAIN, 18));
 		lbPassword.setBounds(219, 201, 76, 27);
-		frame.getContentPane().add(lbPassword);
+		loginFrame.getContentPane().add(lbPassword);
 		
 
 		JTextField tfAccountName = new JTextField();
 		tfAccountName.setFont(new Font("Roboto Condensed", Font.PLAIN, 18));
 		tfAccountName.setBounds(345, 146, 320, 26);
-		frame.getContentPane().add(tfAccountName);
+		loginFrame.getContentPane().add(tfAccountName);
 		tfAccountName.setColumns(10);
 		
 		JPasswordField passwordField = new JPasswordField();
 		passwordField.setFont(new Font("Roboto Condensed", Font.PLAIN, 18));
 		passwordField.setBounds(345, 201, 320, 26);
-		frame.getContentPane().add(passwordField);
+		loginFrame.getContentPane().add(passwordField);
 		
 		JButton btnAdmin = new JButton("Login as Admin");
+		btnAdmin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdminSection.main(new String[] {});
+				loginFrame.dispose();
+			}
+		});
+		
 		btnAdmin.setFont(new Font("Roboto Condensed", Font.PLAIN, 18));
 		btnAdmin.setBounds(430, 282, 164, 43);
-		frame.getContentPane().add(btnAdmin);
+		loginFrame.getContentPane().add(btnAdmin);
 		
 		JButton btnLibrarian = new JButton("Login as Librarian");
 		btnLibrarian.setFont(new Font("Roboto Condensed", Font.PLAIN, 18));
 		btnLibrarian.setBounds(202, 282, 164, 43);
-		frame.getContentPane().add(btnLibrarian);
+		loginFrame.getContentPane().add(btnLibrarian);
 	}
 }
