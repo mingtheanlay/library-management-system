398bf0b3a946c18f822c404ffabf014ecbb280a9
Thean <mingthean.lay@gmail.com>
2020-03-02 16:42:33 +0700

    delete form

--- src/com/library/admin/db/DB.java
+++ src/com/library/admin/db/DB.java
@@ -7,7 +7,7 @@ public class DB {
 		Connection con=null;
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/book","root","eIHWYd9DeqWqdQzA");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/book","root","root0921");
 		}catch(Exception e){System.out.println(e);}
 		return con;
 	}
